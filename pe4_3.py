46e3406ede17780e8c730b5e22401b461c7223e0
timraiser <tim.raiser@student.hu.nl>
2018-09-20 15:09:57 +0200

    huiswerk

---
+++ pe4_3.py
@@ -0,0 +1,4 @@
+uurloon = float(input("Wat verdien je per uur:"))
+uren = int(input("Hoeveel uur heb je gewerkt:"))
+loon = uurloon * uren
+print(str(uren) + " Uur werken levert " + (str(loon)) + " Euro op")
